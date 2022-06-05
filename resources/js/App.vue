f58297ad94a347f87747f3706bde728513d75c75
Felix Masomera <felixgerald3@gmail.com>
2022-06-05 20:23:22 +0200

    Presentation Commit

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container">
         <div class="text-center" style="margin: 20px 0px 20px 0px;">
-            <a href="https://shouts.dev/" target="_blank"><img src="https://i.imgur.com/Nt3kJXa.png"></a><br>
-            <span class="text-secondary">Laravel SPA with Vue 3, Auth (Sanctum), CURD Example</span>
+            <a href="#" target="_blank">Tasks</a><br>
+            <span class="text-secondary">Laravel SPA with Vue 3</span>
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
